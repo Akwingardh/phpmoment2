1aa089bd7fdd702de5111c5210b1cf59fe50b377
Viktor Myhrberg <viktor.myhrberg@hotmail.se>
2023-03-13 12:06:44 +0100

    Final commit

--- variabler.php
+++ variabler.php
@@ -7,9 +7,11 @@
 ?>
                         </section>
             <section class="body-answer">
-                <p><?php echo "Hej! Jag heter " . $name . ", är " . $age . " år gammal och nås på följande e-post: " . $mail;?></p>
+                <p><?php echo "Hej! Jag heter " . $name . ", är " . $age . " år gammal och nås på följande e-post: <a href='mailto:" . $mail . "'>$mail</a>";?></p>
             </section>
 
     <?php 
     include("includes/footer.php");
 ?>
+
+"<a href='mailto:" . $to . "?body=" . $body . "'>"
